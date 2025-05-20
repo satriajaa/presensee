fc42eac682400e9209421339d67a646c51cd93e3
dinoarr <dinoananda.restyanto@gmail.com>
2025-05-20 16:02:23 +0700

    kelas controller

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -12,7 +12,7 @@ class KelasController extends Controller
      */
     public function index()
     {
-        //
+        return Kelas::all();
     }
 
     /**
@@ -20,7 +20,7 @@ class KelasController extends Controller
      */
     public function create()
     {
-        //
+        return view('kelas.create');
     }
 
     /**
@@ -28,38 +28,58 @@ class KelasController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'tingkat' => 'required|integer',
+            'nama_kelas' => 'required|string|max:20',
+        ]);
+
+        Kelas::create($request->all());
+
+        return redirect()->route('kelas.index')->with('success', 'Data kelas berhasil ditambahkan.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Kelas $kelas)
+    public function show($id)
     {
-        //
+        $kelas = Kelas::findOrFail($id);
+        return view('kelas.show', compact('kelas'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Kelas $kelas)
+    public function edit($id)
     {
-        //
+        $kelas = Kelas::findOrFail($id);
+        return view('kelas.edit', compact('kelas'));    
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Kelas $kelas)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'tingkat' => 'required|integer',
+            'nama_kelas' => 'required|string|max:20',
+        ]);
+
+        $kelas = Kelas::findOrFail($id);
+        $kelas->update($request->all());
+
+        return redirect()->route('kelas.index')->with('success', 'Data kelas berhasil diperbarui.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Kelas $kelas)
+    public function destroy($id)
     {
-        //
+        $kelas = Kelas::findOrFail($id);
+        $kelas->delete();
+
+        return redirect()->route('kelas.index')->with('success', 'Data kelas berhasil dihapus.');
     }
 }
